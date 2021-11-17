ab6e799e1b7aabac144d792fb72e67d2cba9f6ad
UnknowUser50 <66477027+UnknowUser50@users.noreply.github.com>
2021-11-17 17:49:11 +0100

    Edition $path & path_r
    Modification des variables selon les utilisateurs (chemins de stockage différents en fonction des utilisateurs)
    Variables ciblées :
    -> $path_q 
    -> $path_r

--- Révisions/Questionnaire-reseau.ps1
+++ Révisions/Questionnaire-reseau.ps1
@@ -2,8 +2,8 @@ function QCM_start {
     # Nettoyage de la console utilisateur :
     Clear-Host
     # Script de QCM
-    $path_q = 'a definir'
-    $path_r = 'a definir'
+    $path_q = 'a definir' # Selon l'emplacement de stockage de votre fichier questions.txt
+    $path_r = 'a definir' # Selon l'emplacement de stockage de votre fichier reponse.txt
     $boucle = Read-Host "> Combien de question(s) voulez-vous ?"
     if ($boucle -eq "" -or $boucle -eq $null)
     {
@@ -16,10 +16,10 @@ function QCM_start {
         # On procéde au traitement :
         $random = Get-Random -Minimum 1 -Maximum 43
         # Recup. question et rép.
-        $question = Get-Content 'a definir' | Select-Object -Index $random
-        $reponse = Get-Content 'a definir' | Select-Object -Index $random | ForEach-Object { $_.split("=")[1] }
+        $question = Get-Content $path_q | Select-Object -Index $random
+        $reponse = Get-Content $path_r | Select-Object -Index $random | ForEach-Object { $_.split("=")[1] }
         # Afin de pas avoir plusieurs fois le même question, nous stockons la valeur de la réponse, nous créons un tableau pour le stocker
-        $num = Get-Content 'a definir' | Select-Object -Index $random | ForEach-Object { $_.split("=")[0] } 
+        $num = Get-Content $path_r | Select-Object -Index $random | ForEach-Object { $_.split("=")[0] } 
         $stock_num = @()
         if( $stock_num -notcontains $num) {
             # La valeur n'est pas présente dans le tableau, nous pouvons donc poser la question :

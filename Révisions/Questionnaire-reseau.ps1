7002b12e1658a2423ca5eb159fb478853bc09003
UnknowUser50 <66477027+UnknowUser50@users.noreply.github.com>
2021-11-17 16:15:04 +0100

    Update Questionnaire-reseau.ps1

--- Révisions/Questionnaire-reseau.ps1
+++ Révisions/Questionnaire-reseau.ps1
@@ -16,11 +16,11 @@ function QCM_start {
         # On procéde au traitement :
         $random = Get-Random -Minimum 1 -Maximum 43
         # Recup. question et rép.
-        $question = Get-Content $path_q | Select-Object -Index $random
-        $reponse = Get-Content $path_r | Select-Object -Index $random | ForEach-Object { $_.split("=")[1] }
+        $question = Get-Content 'a definir' | Select-Object -Index $random
+        $reponse = Get-Content 'a definir' | Select-Object -Index $random | ForEach-Object { $_.split("=")[1] }
         # Afin de pas avoir plusieurs fois le même question, nous stockons la valeur de la réponse, nous créons un tableau pour le stocker
-        $num = Get-Content $path_r | Select-Object -Index $random | ForEach-Object { $_.split("=")[0] } 
-        Get-Content $path_r | Select-Object -Index $random | ForEach-Object { $_.split("=")[0] } >> C:\Users\Hugo\Desktop\num_q.txt 
+        $num = Get-Content 'a definir' | Select-Object -Index $random | ForEach-Object { $_.split("=")[0] } 
+        Get-Content 'a definir' | Select-Object -Index $random | ForEach-Object { $_.split("=")[0] } >> C:\Users\Hugo\Desktop\num_q.txt 
         $stock_num = @()
         if( $stock_num -notcontains $num) {
             # La valeur n'est pas présente dans le tableau, nous pouvons donc poser la question :
